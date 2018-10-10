e77e4e228bbbf976bd27a71ffc0eae7b6ffe85ac
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-10-10 16:41:10 -0500

    Fixed Excel Part

---
+++ ask_assets/click.js
@@ -0,0 +1,12 @@
+
+const f = document.getElementById("ee1");
+const s = document.getElementById("ee2");
+function c1() {
+    f.style.zIndex = 1;
+    s.style.zIndex = -1;
+}
+
+function c2() {
+    f.style.zIndex = -1;
+    s.style.zIndex = 1;
+}
