17787fb698dd5ee37a0ed8889a0efc881e9ac1c3
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-10-10 11:21:26 -0500

    Folder Structure

--- ask.js
+++ ask.js
@@ -1,22 +1,15 @@
-
-var express = require('express')
-var http = require('http')
-var path = require('path')
-let https = require('https');
-
-
+const express = require('express')
+const path = require('path')
 const app = express()
-
-
-app.set( 'port', ( process.env.PORT || 8800 ));
-
-app.set('views', path.join(__dirname, '/views'))
-app.use(express.static(__dirname + '/ask_assets/'));
-
 const routes = require('./controllers')
-app.use('/', routes) 
+
+app.set('port', (process.env.PORT || 8800));
+app.set('views', path.join(__dirname, '/views'))
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules/')))
+app.use(express.static(__dirname + '/ask_assets/'))
+app.use('/', routes)
 
 // Start node server
-app.listen( app.get( 'port' ), function() {
-    console.log( 'Node server is running on port ' + app.get( 'port' ));
-    });
+app.listen(app.get('port'), function () {
+    console.log('Node server is running on port ' + app.get('port'));
+});
