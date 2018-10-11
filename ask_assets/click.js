e8891757d92f6e1a126fef5dd09c741711b4d22b
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-10-11 16:10:47 -0500

    Fixed navigation colors

--- ask_assets/click.js
+++ ask_assets/click.js
@@ -1,12 +1,17 @@
 
 const f = document.getElementById("ee1");
 const s = document.getElementById("ee2");
-function c1() {
+// function c1(a,b) {
+//     document.getElementById(a).style.zIndex = "2"
+//     document.getElementById(b).style.zIndex = "-2"
+// }
+
+function c2() {
     f.style.zIndex = 1;
     s.style.zIndex = -1;
 }
 
-function c2() {
+function c1() {
     f.style.zIndex = -1;
     s.style.zIndex = 1;
 }
