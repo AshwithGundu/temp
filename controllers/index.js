140de30b0e47e8e17a48e1f9004ad026c11cf527
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-10-16 17:44:28 -0500

    fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,8 @@
 var express = require('express')
-var http = require('http')
-var path = require('path')
-let https = require('https');
 const app = express.Router()
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var w = require('./weather');
+let nodemail = require('./mail');
+
 
 app.use(bodyParser());
 
@@ -16,24 +13,6 @@ app.get('/', (req, res) => {
     });
 });
 
-
-async function fn() {
-var lat = 17.3875977;
-var lon = 78.4922316;
-var wURL = `https://api.darksky.net/forecast/40283bcf75c8e68c566ab2f7a029985a/${lat},${lon}`;
-console.log(res.data.results[0].formatted_address);
-return fetch.get(wURL).then((res) => {
-    var temp = res.data.currently.temperature;
-    var aTemp = res.data.currently.apparentTemperature;
-
-    console.log(`it is ${temp}.It feels like ${aTemp}`);
-});
-
-
-}
-
-
-
-
+app.post('/sendmail', nodemail.nm);
 
 module.exports = app
