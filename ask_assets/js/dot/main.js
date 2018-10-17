d02fd25c7a332043f423ee11ccb4a3e4a57fc6b4
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-10-16 22:19:41 -0500

    New Navigation

--- ask_assets/js/dot/main.js
+++ ask_assets/js/dot/main.js
@@ -1,49 +1,57 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+	var scrolling = false;
 	var contentSections = $('.cd-section'),
-		navigationItems = $('#cd-vertical-nav a');
+		verticalNavigation = $('.cd-vertical-nav'),
+		navigationItems = verticalNavigation.find('a'),
+		navTrigger = $('.cd-nav-trigger'),
+		scrollArrow = $('.cd-scroll-down');
 
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
+	$(window).on('scroll', checkScroll);
+
+	//smooth scroll to the selected section
+	verticalNavigation.on('click', 'a', function (event) {
+		event.preventDefault();
+		smoothScroll($(this.hash));
+		verticalNavigation.removeClass('open');
 	});
 
-	//smooth scroll to the section
-	navigationItems.on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
-    //smooth scroll to second section
-    $('.cd-scroll-down').on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-    });
+	//smooth scroll to the second section
+	scrollArrow.on('click', function (event) {
+		event.preventDefault();
+		smoothScroll($(this.hash));
+	});
 
-    //open-close navigation on touch devices
-    $('.touch .cd-nav-trigger').on('click', function(){
-    	$('.touch #cd-vertical-nav').toggleClass('open');
-  
-    });
-    //close navigation on touch devices when selectin an elemnt from the list
-    $('.touch #cd-vertical-nav a').on('click', function(){
-    	$('.touch #cd-vertical-nav').removeClass('open');
-    });
+	// open navigation if user clicks the .cd-nav-trigger - small devices only
+	navTrigger.on('click', function (event) {
+		event.preventDefault();
+		verticalNavigation.toggleClass('open');
+	});
 
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
-			}
+	function checkScroll() {
+		if (!scrolling) {
+			scrolling = true;
+			(!window.requestAnimationFrame) ? setTimeout(updateSections, 300): window.requestAnimationFrame(updateSections);
+		}
+	}
+
+	function updateSections() {
+		var halfWindowHeight = $(window).height() / 2,
+			scrollTop = $(window).scrollTop();
+		contentSections.each(function () {
+			var section = $(this),
+				sectionId = section.attr('id'),
+				navigationItem = navigationItems.filter('[href^="#' + sectionId + '"]');
+			((section.offset().top - halfWindowHeight < scrollTop) && (section.offset().top + section.height() - halfWindowHeight > scrollTop)) ?
+			navigationItem.addClass('active'): navigationItem.removeClass('active');
 		});
+		scrolling = false;
 	}
 
 	function smoothScroll(target) {
-        $('body,html').animate(
-        	{'scrollTop':target.offset().top},
-        	600
-        );
+		$('body,html').animate({
+				'scrollTop': target.offset().top
+			},
+			300
+		);
 	}
 });
